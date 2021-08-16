5fe4c3dd00d8663e3689419756e0084f9e5f43b1
Christian Santos <csantos1517@gmail.com>
2021-08-16 16:39:04 +0800

    Assigned DockLayout to other constructor

--- VB/E2502/MyDockManager/MyDockPanel.vb
+++ VB/E2502/MyDockManager/MyDockPanel.vb
@@ -12,6 +12,7 @@ Namespace WindowsFormsApplication1
 
         Protected Friend Sub New(ByVal createControlContainer As Boolean, ByVal dock As DockingStyle, ByVal dockManager As DockManager)
             MyBase.New(createControlContainer, dock, dockManager)
+            DockLayout = New MyDockLayout(dock, Me)
         End Sub
     End Class
 End Namespace
